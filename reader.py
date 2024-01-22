46e2d3a665d844fb9094c2f72f4b52bcafc5749b
Clement Helsens <clementhelsens>
2024-01-22 12:53:41 +0100

    updates

--- reader.py
+++ reader.py
@@ -15,21 +15,52 @@ def tifreader(path):
 #    print(image.shape)
 #    return image
 
+
+
 #_______________________________________________
-def nd2reader(path):
-    #https://github.com/cwood1967/napari-nikon-nd2/blob/main/napari_nikon_nd2/_reader.py
-    stack = nd2.ND2Reader(path)
+def nd2reader_getSampleMetadata(path):
+    stack = nd2.reader.ND2Reader(path)
+    metadata = stack.metadata
+    metadata_dict={}
+    metadata_dict['number_of_frames']       = metadata['num_frames']
+    metadata_dict['number_of_channels']     = len(metadata['channels'])
+    metadata_dict['experiment_description'] = metadata['experiment']['description']
+    metadata_dict['name_of_channels']       = ''
+    metadata_dict['date']                   = metadata['date']
+    
+    for i in range(len(metadata['channels'])):
+        if i<len(metadata['channels'])-1: metadata_dict['name_of_channels'] += metadata['channels'][i]+', '
+        else: metadata_dict['name_of_channels'] += metadata['channels'][i]
+    
+    print('stack.metadata = ',stack.metadata)
+    return metadata_dict
+
+#_______________________________________________
+def nd2reader_getFrameMetadata(path):
+    stack = nd2.reader.ND2Reader(path)
+    metadata = stack.metadata
+    timesteps = stack.timesteps.tolist()
+    metadata_dict={}
+    metadata_dict['time'] = timesteps
+    metadata_dict['x_pos'] = metadata['x_coordinates']
+    metadata_dict['y_pos'] = metadata['y_coordinates']
+    metadata_dict['z_pos'] = metadata['z_coordinates']
+    metadata_dict['height'] = metadata['height']
+    metadata_dict['width'] = metadata['width']
+
+    return metadata_dict
+
+
+#_______________________________________________
+def nd2reader_getFrames(path):
+    stack = nd2.reader.ND2Reader(path)
+    metadata = stack.metadata
     sizes = stack.sizes
-    print (sizes)
-    print('--',len(stack))
     if 't' not in sizes: sizes['t'] = 1
     if 'z' not in sizes: sizes['z'] = 1
     if 'c' not in sizes: sizes['c'] = 1
-    print (sizes)    
-
     stack.bundle_axes = 'zcyx'
     stack.iter_axes = 't'
-    print(stack.metadata)
     n = len(stack)
 
     shape = (sizes['t'], sizes['z'], sizes['c'], sizes['y'], sizes['x'])
@@ -37,9 +68,50 @@ def nd2reader(path):
 
     for i in range(n):
         image[i] = stack.get_frame(i)
-        #print(stack.get_frame(i).metadata)
+    image = np.squeeze(image)
+    print('image shape in nd2raeader: ',image.shape)
+    stack.close()
+    return image, metadata['channels']
+
+#_______________________________________________
+def nd2reader(path):
+    #https://github.com/cwood1967/napari-nikon-nd2/blob/main/napari_nikon_nd2/_reader.py
+    stack = nd2.reader.ND2Reader(path)
+    sizes = stack.sizes
+    print ('sizes=',sizes)
+    print ('stack=',stack)
+    print('stack type=',type(stack))
+    print('events=',stack.events)
+    print('f rate---------------',stack.frame_rate)
+    print('f timesteps---------------',stack.timesteps/60000.)
+    print('len stack',len(stack))
+    print('stack.metadata = ',stack.metadata)
+
+    #parser=nd2.parser.Parser(path)
+    #print('parser=',parser)
+    #metadata=nd2.raw_metadata.RawMetadata(path)
+    #print(metadata)
+    #sys.exit(3)
+    #print('--',len(stack))
+    if 't' not in sizes: sizes['t'] = 1
+    if 'z' not in sizes: sizes['z'] = 1
+    if 'c' not in sizes: sizes['c'] = 1
+    print (sizes)    
+
+    stack.bundle_axes = 'zcyx'
+    stack.iter_axes = 't'
+    #print(stack.metadata)
+    n = len(stack)
+
+    shape = (sizes['t'], sizes['z'], sizes['c'], sizes['y'], sizes['x'])
+    image  = np.zeros(shape, dtype=np.float32)
+
+    for i in range(n):
+        image[i] = stack.get_frame(i)
+        print('-------------- i=',i,'    ',stack.get_frame(i).metadata)
     image = np.squeeze(image)
     print(image.shape)
+    stack.close()
     return image
 
 
--- segmentationTools.py
+++ segmentationTools.py
@@ -1,4 +1,4 @@
-from skimage.measure import label, regionprops
+from skimage.measure import label, regionprops, find_contours
 from skimage.morphology import disk, binary_closing, binary_dilation
 from scipy.ndimage import binary_fill_holes
 import numpy as np
@@ -6,6 +6,8 @@ import numba as nb
 from numba.types import bool_
 import pyclesperanto_prototype as cle
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('agg')
 
 import json
 import glob, os, sys, math
@@ -15,40 +17,58 @@ import apoc
 gpu = False
 sigma = 50
 minimum_size=250
+version='0.0.1'
 
-#_______________________________________________
-def normalize_background(img, sigma, gpu):
+
+class customLocalThresholding_Segmentation:
+    def __init__(self, segchannel, channels, threshold=2., delta=1, npix_min=400, npix_max=4000):
+        self.segchannel = segchannel
+        self.channels   = channels
+        self.threshold  = threshold
+        self.delta      = delta
+        self.npix_min   = npix_min
+        self.npix_max   = npix_max
+        self.algorithm_parameters = {'segchannel':self.channels[self.segchannel],'threshold':threshold, 'delta':delta, 'npix_min':npix_min, 'npix_max':npix_max}
+        self.algorithm_type       = 'localthresholding'
+        self.algorithm_version    = 'main'
+
+    def get_AlgoID(self):
+        algorithm_parameters="" 
+        for par in self.algorithm_parameters:
+            algorithm_parameters+=par+str(self.algorithm_parameters[par])
+        
+        algoid = "algorithm_type={0}__algorithm_version={1}__algorithm_parameters={2}".format(self.algorithm_type, self.algorithm_version, algorithm_parameters) 
+        return algoid
     
-    """
-    This function loads an image and performs the division of the input by a blurred filtered version of itself.
-    """
-    intensity_normalized = None
-    
-    if gpu:
-        pushed = cle.push(img)
-        intensity_normalized = cle.divide_by_gaussian_background(pushed, result_substract, sigma, sigma, 0)
-        intensity_normalized = np.asarray(intensity_normalized) #img is pulled from GPU memory
-    
-    else:
-        intensity_normalized = cle.divide_by_gaussian_background(img, intensity_normalized, sigma,sigma,0)
-        intensity_normalized = np.asarray(intensity_normalized)
-    
-    return intensity_normalized
-#_______________________________________________
-class NpEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        if isinstance(obj, np.floating):
-            return float(obj)
-        if isinstance(obj, np.ndarray):
-            return obj.tolist()
-        return super(NpEncoder, self).default(obj)
+
+    #_______________________________________________
+    def segmentation(self, img):
+        image=img[self.segchannel]
+        img_seeds=fastiter(image, self.delta, self.threshold)
+
+        #dilated = binary_dilation(img_seeds, disk(2))
+        closed = binary_closing(img_seeds, disk(4))
+        filled = binary_fill_holes(closed).astype(int)
+        label_im = label(filled)
+
+        regions=regionprops(label_im)
+        contours=[]
+        for r in regions:
+        
+            if r.num_pixels>self.npix_min and r.num_pixels<self.npix_max:
+                contours.append(r)
+
+        contour_list=build_contour_dict(contours, image, img, self.channels)
+        for c in contour_list:
+            c['algorithm_parameters'] = self.algorithm_parameters
+            c['algorithm_type']       = self.algorithm_type
+            c['algorithm_version']    = self.algorithm_version
+        return contour_list
 
 
 #_______________________________________________
 @nb.njit(fastmath = True)
-def fastiter(image, thr=2., delta=1):
+def fastiter(image, delta, threshold):
     img_seeds=np.zeros(image.shape, dtype=bool_)
     for i in range(image.shape[0]):
         bkg=[]
@@ -67,13 +87,103 @@ def fastiter(image, thr=2., delta=1):
                     if jd+j<0 or jd+j>image.shape[1]-1:continue
                     sig.append(image[i+id][j+jd])
 
-            if np.std(np.array(sig))>thr*std:
+            if np.std(np.array(sig))>threshold*std:
                 img_seeds[i][j]=True
     return img_seeds
 
 
 #_______________________________________________
-def customLocalThresholding_Segmentation(img,  outdir, thr=2., delta=1, npix=400):
+def build_contour_dict(contours, image, img, channels):
+
+    out_contours=[]
+    for c in range(len(contours)):
+        single_pixels_inside={'x':[], 'y':[], 'z':[], 'intensity':{}}
+        all_pixels_inside={'sum_intensity':{}, 'std_intensity':{}, 'mean_intensity':{}}
+        single_pixels_contour={'x':[], 'y':[], 'z':[], 'intensity':{}}
+        all_pixels_contour={'sum_intensity':{}, 'std_intensity':{}, 'mean_intensity':{}}
+
+        mask0=np.zeros(image.shape, dtype=bool)
+
+        for ch in channels:
+            single_pixels_inside['intensity'][ch]=[]
+            all_pixels_inside['sum_intensity'][ch]=0
+            all_pixels_inside['std_intensity'][ch]=0
+            all_pixels_inside['mean_intensity'][ch]=0
+
+            single_pixels_contour['intensity'][ch]=[]
+            all_pixels_contour['sum_intensity'][ch]=0
+            all_pixels_contour['std_intensity'][ch]=0
+            all_pixels_contour['mean_intensity'][ch]=0
+
+        for p in contours[c].coords:
+            mask0[p[0]][p[1]]=True
+            single_pixels_inside['x'].append(int(p[0]))
+            single_pixels_inside['y'].append(int(p[1]))
+            for ch in range(len(channels)):
+                inttmp=0
+                if len(p)==2: inttmp=float(img[ch][p[0]][p[1]])
+                if len(p)==3: inttmp=float(img[ch][p[0]][p[1]][p[2]])
+                single_pixels_inside['intensity'][channels[ch]].append(inttmp)
+                all_pixels_inside['sum_intensity'][channels[ch]]+=inttmp
+            if len(p)==3:
+                single_pixels_inside['z'].append(int(p[2]))
+
+        cs=plt.contour(mask0, [0.5],linewidths=1.2,  colors='red')
+        contcoords = cs.allsegs[0][0]
+        z_flag=False
+        if contcoords.shape[1]==3: z_flag=True
+        for p in contcoords:
+            single_pixels_contour['x'].append(p[0])
+            single_pixels_contour['y'].append(p[1])
+            if z_flag:single_pixels_contour['z'].append(p[2])
+            for ch in range(len(channels)):
+                inttmp=0
+                if not z_flag: inttmp=float(img[ch][int(p[0])][int(p[1])])
+                if z_flag: inttmp=float(img[ch][int(p[0])][int(p[1])][int(p[2])])
+                single_pixels_contour['intensity'][channels[ch]].append(inttmp)
+                all_pixels_contour['sum_intensity'][channels[ch]]+=inttmp
+
+        for ch in range(len(channels)):
+            all_pixels_inside['mean_intensity'][channels[ch]]=np.mean(single_pixels_inside['intensity'][channels[ch]])
+            all_pixels_inside['std_intensity'][channels[ch]]=np.std(single_pixels_inside['intensity'][channels[ch]])
+            all_pixels_contour['mean_intensity'][channels[ch]]=np.mean(single_pixels_contour['intensity'][channels[ch]])
+            all_pixels_contour['std_intensity'][channels[ch]]=np.std(single_pixels_contour['intensity'][channels[ch]])
+
+        center={'x':contours[c].centroid[0],'y':contours[c].centroid[1],'z':0}
+        if len(contours[c].centroid)==3:
+            center={'x':contours[c].centroid[0],'y':contours[c].centroid[1],'z':contours[c].centroid[2]}
+
+        contour_dic={
+            'center':center,
+            'number_of_pixels':contours[c].num_pixels,
+            'single_pixels_inside':single_pixels_inside,
+            'all_pixels_inside':all_pixels_inside,
+            'single_pixels_contour':single_pixels_contour,
+            'all_pixels_contour':all_pixels_contour,
+        }
+        out_contours.append(contour_dic)
+    return out_contours
+
+
+########################################################################################################
+########################################################################################################
+########################################################################################################
+########################################################################################################
+
+
+#_______________________________________________
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+    
+#_______________________________________________
+def customLocalThresholding_Segmentation_out(img,  outdir, thr=2., delta=1, npix_min=400, npix_max=4000):
 
     for im in range(len(img)):
         image=img[im][0]
@@ -87,7 +197,7 @@ def customLocalThresholding_Segmentation(img,  outdir, thr=2., delta=1, npix=400
         regions=regionprops(label_im)
         cells=[]
         for r in regions:
-            if r.area>npix:
+            if r.area>npix_min and r.area<npix_max:
                 cells.append(r)
     
         previous_labels=[]
@@ -173,10 +283,31 @@ def customLocalThresholding_Segmentation(img,  outdir, thr=2., delta=1, npix=400
                 outfiletf.write(jsontf_object)
 
 
+#_______________________________________________
+def normalize_background(img, sigma, gpu):
+    
+    """
+    This function loads an image and performs the division of the input by a blurred filtered version of itself.
+    """
+    intensity_normalized = None
+    
+    if gpu:
+        pushed = cle.push(img)
+        intensity_normalized = cle.divide_by_gaussian_background(pushed, result_substract, sigma, sigma, 0)
+        intensity_normalized = np.asarray(intensity_normalized) #img is pulled from GPU memory
+    
+    else:
+        intensity_normalized = cle.divide_by_gaussian_background(img, intensity_normalized, sigma,sigma,0)
+        intensity_normalized = np.asarray(intensity_normalized)
+    
+    return intensity_normalized
+
+
+
 
 #_______________________________________________
-def apoc_Segmentation(img, outdir, npix=300):
-    clf = apoc.PixelClassifier(opencl_filename="models/pixel_classification.cl")
+def apoc_Segmentation(img, outdir, npix=300, model="models/pixel_classification.cl"):
+    clf = apoc.PixelClassifier(opencl_filename=model)
 
     image=img[:,0,:,:]
     normalize = normalize_background(image, sigma, gpu)
